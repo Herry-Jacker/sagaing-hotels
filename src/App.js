06f830d6d6fccabe6dfd2e79506321343c31293c
Soe Paing <herryjacker4@gmail.com>
2024-03-13 07:54:36 +0630

    added: hotels' images

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Nav from './components/nav/Nav';
 import Home from './pages/home/Home';
 
 // Hotel banner Images
-import HotelImg from './statics/hotel1.jpg'
+import HotelImg1 from './statics/hotel1.jpg'
+import HotelImg2 from './statics/hotel2.jpg'
+import HotelImg3 from './statics/hotel3.jpg'
+import HotelImg4 from './statics/hotel4.jpg'
+import HotelImg5 from './statics/hotel5.jpg'
+import HotelImg6 from './statics/hotel6.jpg'
+import HotelImg7 from './statics/hotel7.jpg'
 // Hotel banner Images
 
 export const HotelsContext = createContext(null);
 
 const hotels = [
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg},
-  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg}
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg1},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg2},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg3},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg4},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg5},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg6},
+  {name: 'Example Hotel One', description: 'Example Something More Like This Is The Best', link: '#', bannerImage: HotelImg7}
 ]
 
 export default function App() {
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,10 @@ import './home.css';
 import ImageSlider from '../../components/shares/imageSlider/ImageSlider';
 
 const Banner = () => {
-    const images = [
-        'https://img.freepik.com/free-photo/nature-water-beautiful-blue-house_1203-4911.jpg?t=st=1710034435~exp=1710038035~hmac=22534bc707375fd60bad33b06151823dbe494901d75d683c9c4c313cdb26a956&w=900',
-        'https://img.freepik.com/free-photo/light-garden-luxury-pool-nature_1203-4908.jpg?t=st=1710034532~exp=1710038132~hmac=da8336237043e3dfc7968772f6b8cab64c594f108d9ad5b4ead4940f6f61207d&w=900',
-        'https://img.freepik.com/free-photo/nature-water-beautiful-blue-house_1203-4911.jpg?t=st=1710034435~exp=1710038035~hmac=22534bc707375fd60bad33b06151823dbe494901d75d683c9c4c313cdb26a956&w=900',
-      ];
     
       return (
         <div>
-          <ImageSlider images={images} />
+          <ImageSlider/>
         </div>
       );
     
